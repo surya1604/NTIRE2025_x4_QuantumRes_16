76598e573275a9887afa35347b4ff5c561f9dc1d
Surya Vashisth <vashisth.surya33@gmail.com>
2025-03-27 20:52:13 +0530

    Initial Commit

---
+++ run.sh
@@ -0,0 +1,2 @@
+CUDA_VISIBLE_DEVICES=0 python test_demo.py --data_dir /home/sachinchaudhary/NTIRE 2025/NTIRE2024_ImageSR_x4-main/div2k --save_dir /home/sachinchaudhary/NTIRE 2025/NTIRE2024_ImageSR_x4-main/div2k --model_id 0
+
